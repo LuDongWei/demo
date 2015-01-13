5c72023df891f608963b3d0fe8ff6a97a5c5af38
Mr.lu <weixiaobao5603169@gmail.com>
2015-01-13 20:24:20 +0800

    gai

--- bulb/src/bulb.js
+++ bulb/src/bulb.js
@@ -11,26 +11,38 @@ $("#pageSlider").bxSlider({
   controls: false
 });
 
+
 //3f - scenes
+var width = $(".floor-3").width(),
+    big =  width*0.4,    
+    small = width*0.2;
+
+$(window).on("resize",function(){
+
+	width = $(".floor-3").width(),
+    big =  width*0.4,    
+    small = width*0.2;
+
+})    
 
 $("#scenesList").find("li").on('mouseover',function(){
 
 		$("#f3Text").hide();
 
-		$(this).stop().animate({"width":"40%"},400,function(){
+		$(this).stop(!0, !0).animate({"width":big},400,function(){
 		   isMove = true;
 		});
 
 		$(this).find(".scene-content").addClass("selected");
 
-		$("#scenesList li").not(this).stop().animate({"width":"20%"},395);
+		$("#scenesList li").not(this).stop(!0, !0).animate({"width":small},400);
 		$("#scenesList li").not(this).find(".scene-content").removeClass("selected"); 
 	    
 })
 
 
 $("#scenesList").on('mouseleave',function(){
-	  $("#scenesList li").stop().animate({"width":"25%"},400,function(){
+	  $("#scenesList li").stop(!0, !0).animate({"width":"25%"},400,function(){
            $("#f3Text").show(); 
 	  });
 	  $("#scenesList li").find(".scene-content").removeClass("selected"); 
