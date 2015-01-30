2c488ef4d819322bf3f4cb1c535750d11a0ae05e
LuDongWei <weixiaobao5603169@gmail.com>
2015-01-30 10:08:13 +0800

    gai

--- node-address/address.js
+++ node-address/address.js
@@ -1,60 +1,104 @@
 var express = require('express');
+var cors = require('cors');
+var url = require('url');
 var addressModel = require('./addressModel');
 var addressApp = express();
+var router = express.Router();
+
+addressApp.use(cors());
+
+var model = new addressModel(); 
+
+// addressApp.param('/',function(req,res,next,id){
+//        req.item = {
+//        	   id : id ,
+//        	   name : "hah",
+//        	   phone : "1231"
+//        };
+//        next(); 
+// })
 
 
-var model = new addressModel();
- 
-addressApp.listen(8080); 
-
-//查询
-addressApp.get('/get/addressList', function(req, res) {
-    
-	model.getAddress(function(data) {
-		res.jsonp({
-			addressList : data
-		})
-	})
-
+router.route('/')
+.all(function(req,res,next){
+    console.log(req.method)
+    next();
 })
+.get(function(req,res,next){     //查询
+	if (req.method === "GET") {
+        model.getAddress(function(data) { 
+		  res.jsonp(data)
+	    })
+	}else{
+        next(); 
+	}
+}) 
+.post(function(req,res,next){     //增加
+    if (req.method === "POST") {
+        console.log(req.params)
+        console.log(req.body)
+        console.log(req.query)
+        console.log(req.originalUrl)
+
+    	res.jsonp({
+    		return : false
+    	})
+        //console.log(req.body.name)
+
+		// var pathname = req.params['0'],
+		// 	name = pathname.split("/")[0],
+		// 	phone = pathname.split("/")[1];
+
+		// model.addAddress(name, phone, function(data) {
+
+		// 	if (data) {
+		// 		res.jsonp(data)
+		// 	}
+		// })
+	}else{
+	    next(); 	
+	} 
+})
+.put(function(req,res,next){      //更新
+	 if (req.method === "PUT") {
+		var pathname = req.params['0'],
+			name = pathname.split("/")[0],
+			phone = pathname.split("/")[1],
+			id = pathname.split("/")[2];
+
+		model.updaAddress(name, phone, id, function(data) {
+
+			if (data) {
+				res.jsonp(data)
+			}
+		})
+	 }else{
+        next(); 		
+	 }  
+ 
+})
+.delete(function(req,res,next){    //删除
+
+	if (req.method === "DELETE") {
+		var id = req.params['0'];
+ 
+		model.deleteAddress(id, function(data) {
+
+			if (data) {
+				res.jsonp(data)
+			}
+		})
+	}else{
+	  	res.jsonp({
+	  		return: false
+	  	})
+	}
+
+});
 
 
-
-// addressApp.use('/get/addressList',[list1,list2])
-
-// var aabb=null;
-
-// function list1(req, res, next){
-   
-//    model.getAddress(function(data){
-//        console.log(data)
-
-//        next();
-//    });
-  
-// }
-
-// function list2(req, res, next){
-  
-//   console.log(123) 
-//   console.log(aabb) 
-
-//   res.jsonp({
-// 		error: 'message'
-//   })
-
-//    next();
-// }
-
-// function closeLink(req, res, next){
-//    model.closeAddress();  
-//    next();
-// }
-
-
-
-
-
+addressApp.use('/address', router);
+addressApp.listen(8080); 
 
 
 
--- node-address/addressModel.js
+++ node-address/addressModel.js
@@ -1,4 +1,5 @@
 var mysql      = require('mysql');
+var data = null;
 
 var Model = function() {
     var self = this;
@@ -23,27 +24,108 @@ var Model = function() {
 // });
 
 
-var selectSQL = 'select * from address limit 10';
-
-var insertSQL = 'insert into t_address(name) values("conan"),("fens.me")';
-var deleteSQL = 'delete from t_address';
-var updateSQL = 'update t_address set name="conan update"  where name="conan"';
-
-
+var selectSQL = 'select * from address';
+var insertSQL = 'insert into address (name,phone) values ("$name$",$phone$)';
+var deleteSQL = 'delete from address where id=$id$';
+var updateSQL = 'update address set name="$name$",phone=$phone$ where id=$id$';
 
 //查询
 Model.prototype.getAddress = function(callback) {
     var self = this;
     
-    self.connection.query(selectSQL, function(err0, res0 ) {
+    self.connection.query(selectSQL, function(err0, res0) {
+
         if (err0) {
             console.log(err0);
+            data = {
+                return: false,
+                message: err0
+            }
+        } else {
+            data = res0
         }
-        
-        callback(res0)
+        callback(data)
     })
 
 }
+
+//增加
+Model.prototype.addAddress = function(name,phone,callback) {
+    var self = this;
+        
+    var newInsertSQL = insertSQL.replace('$name$',name).replace('$phone$',phone);  
+
+    self.connection.query(newInsertSQL, function(err0, res0 ) {
+
+        if (err0) {
+            console.log(err0);
+            data = {
+                return: false,
+                message: err0
+            }
+        }else{
+            data = {
+                return : true,
+                message : null
+            }
+        }
+        
+        callback(data)
+    })
+}
+
+//删除
+Model.prototype.deleteAddress = function(id,callback) {
+    var self = this;
+    
+    var newDeleteSQL = deleteSQL.replace('$id$',id);
+
+    self.connection.query(newDeleteSQL, function(err0, res0 ) {
+
+        if (err0) {
+            console.log(err0);
+            data = {
+                return: false,
+                message: err0
+            }
+        }else{
+            data = {
+                return : true,
+                message : null
+            }
+        }
+        
+        callback(data)
+    })
+
+}
+
+//更新
+Model.prototype.updaAddress = function(name,phone,id,callback) {
+    var self = this;
+
+    var newUpdateSQL = updateSQL.replace('$name$',name).replace('$phone$',phone).replace('$id$',id);  
+    
+    self.connection.query(newUpdateSQL, function(err0, res0 ) {
+
+        if (err0) {
+            console.log(err0);
+            data = {
+                return: false,
+                message: err0
+            }
+        }else{
+            data = {
+                return : true,
+                message : null
+            }
+        }
+        
+        callback(data)
+    })
+
+}
+
 
 //关闭连接
 Model.prototype.closeAddress = function() {
