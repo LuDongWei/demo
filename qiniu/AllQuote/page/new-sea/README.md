70b61ab6e242721a5758f0d4ef4e8716d1f3d74b
Mr.lu <weixiaobao5603169@gmail.com>
2015-06-12 17:52:06 +0800

    aa

--- qiniu/AllQuote/page/new-sea/README.md
+++
@@ -1,8 +0,0 @@
-## 项目重构
-1. index.html      首页
-2. submits.html    上传身份证
-3. price.html      价格 
-4. about.html      关于
-5. register.html   注册
-6. login.html      登陆
-7  userIndex.html  用户首页
--- qiniu/AllQuote/page/warehouse/warehouse.js
+++
@@ -1,9 +0,0 @@
-var client = new ZeroClipboard($(".click-copy"));
-
-client.on("aftercopy", function() {
-	alert("复制完成")
-})
-
-client.on("error", function(event) {
-    $(".click-copy").addClass("on-copy");
-});
